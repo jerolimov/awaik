54311b7995de61051d7538d5e9ae3bc43f22d682
Christoph Jerolimov <jerolimov+git+2022@gmail.com>
2022-02-05 00:57:49 +0100

    Add title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,17 @@
+import { MetaFunction } from "remix";
+
+export const meta: MetaFunction = () => {
+  return { title: "Time to awaik" };
+};
+
 export default function Index() {
   return (
-    <div className="p-8 text-2xl">
-      <h1>Welcome to Remix</h1>
+    <div className="p-8 mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+      <h1 className="text-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        {'Time to '}
+        <span className="text-indigo-600">awaik</span>
+        {'.'}
+      </h1>
     </div>
   );
 }
